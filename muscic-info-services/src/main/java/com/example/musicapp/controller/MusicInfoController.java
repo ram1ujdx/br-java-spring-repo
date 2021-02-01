63a58c089e9e29edbf1e62f3981700e2807131d7
ramanujdas <ramanujds9@gmail.com>
2021-02-01 17:19:37 +0530

    CORS Configuration Added

--- muscic-info-services/src/main/java/com/example/musicapp/controller/MusicInfoController.java
+++ muscic-info-services/src/main/java/com/example/musicapp/controller/MusicInfoController.java
@@ -1,6 +1,7 @@
 package com.example.musicapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @RestController
 @RequestMapping("/music")
+//@CrossOrigin
 public class MusicInfoController {
 
 	@Autowired
--- music-api-gateway/src/main/java/com/example/musicapp/MusicApiGatewayApplication.java
+++ music-api-gateway/src/main/java/com/example/musicapp/MusicApiGatewayApplication.java
@@ -1,9 +1,15 @@
 package com.example.musicapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,4 +20,40 @@ public class MusicApiGatewayApplication {
 		SpringApplication.run(MusicApiGatewayApplication.class, args);
 	}
 
+	
+	@Bean
+	public CorsFilter corsFilter() {
+
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+		final CorsConfiguration config = new CorsConfiguration();
+
+//		config.setAllowCredentials(true);
+
+		config.addAllowedOrigin("http://localhost:4200");
+
+//		config.addAllowedHeader("Access-Control-Allow-Origin");
+		config.addAllowedHeader("*");
+
+		config.addAllowedMethod("OPTIONS");
+
+		config.addAllowedMethod("HEAD");
+
+		config.addAllowedMethod("GET");
+
+		config.addAllowedMethod("PUT");
+
+		config.addAllowedMethod("POST");
+
+		config.addAllowedMethod("DELETE");
+
+		config.addAllowedMethod("PATCH");
+
+		source.registerCorsConfiguration("/**", config);
+
+		return new CorsFilter(source);
+
+	}
+	
+	
 }
